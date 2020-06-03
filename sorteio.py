cac2d4369bdfd33b58573029fee3ef265a132473
LucioFSMelo <lucius.mat@gmail.com>
2020-06-03 10:04:48 -0300

    Explicação da Função

---
+++ sorteio.py
@@ -0,0 +1,22 @@
+def sorteio_contagem(r):
+    """Função que sorteia números e os inseri em uma lista para serem contados
+
+    Ex:
+    >>> sorteio(random(inicio, fim))
+    2
+    3
+    4
+    2
+    1
+    3
+    >>> lista[(posição_sorteio, numero)]
+    [2, 3, 4, 2, 1, 3]
+    >>> contagem_rapetidos(número, repetição)
+    1: 1
+    2: 2
+    3: 2
+    4: 1
+
+    :param r: numero a ser sorteado
+
+    """
